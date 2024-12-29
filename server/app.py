24fa62087d79f0767f094ac599a4cee0df7c1241
Daniel Zamanski <daniel.zamanski@gmail.com>
2024-12-29 12:33:01 +0200

    backend skeleton

--- server/app.py
+++ server/app.py
@@ -22,6 +22,17 @@ app.add_middleware(CORSMiddleware,
                     allow_headers=["*"], )    
 
 
+@app.get('/api/{queue_name}?timout={ms}')
+def fetch_message_queue_by_name(queue_name:str,ms:int=10)->dict[str,str|int]:
+       print(queue_name,ms)
+       return {"status":200,"message":"Message"}
+
+@app.post('/api/{queue_name}')
+def add_message_to_queue(queue_name:str,msg:str)->dict[str,str|int]:
+        print(queue_name,msg)
+        return {"status":200,"message":"Message"}
+
+
 if(__name__) == '__main__':
         uvicorn.run(
         "app:app",
